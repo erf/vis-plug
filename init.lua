ffc1fbf3a95170aa06918aecb5670005bd1bc534
Erlend Fagerheim <erlendf80@gmail.com>
2020-02-08 00:45:09 +0100

    use git clone

--- init.lua
+++ init.lua
@@ -1,33 +1,25 @@
 local M = {}
 
 -- plugins directory
-local plugins_dir = 'plugins/'
+local plug_dir = 'plugins/'
 
 -- plugins path
-local plugins_path = os.getenv('HOME') .. '/.config/vis/plugins'
+local plug_path = os.getenv('HOME') .. '/.config/vis/plugins/'
 
--- downlaod and require plugins ( set in visrc.lua )
+-- clone and require plugins ( set in visrc.lua )
 if plugins then 
 	for i, url in ipairs(plugins) do
-		-- -q  > quite
-		-- -nc > dont download if file exists ( even if newer )
-		-- -N  > download if newer
-		-- -P  > set directory prefix
-		os.execute('wget -q -nc -P ' .. plugins_path .. ' ' .. url)
 		local name_ext = url:match('([^/]+)$')
 		local name = name_ext:match('(.+)%..+')
-		local path = plugins_path .. '/' .. name
-		require(plugins_dir .. name)
+		os.execute('git -C ' .. plug_path .. ' clone ' .. url .. ' --quiet')
+		require(plug_dir .. name)
 	end
 end
 
 return M
 
--- TODO 
--- only use repo name ( when plugins have init.lua file we could use git clone to plugins folder )
--- download and overwrite if newer file exists ( use -N )
--- configurable plugin folder
--- commands to download plugin files
--- simplify match statement
+-- TODO's
+-- configure plugin folder
+-- commands to fetch and update plugins
+-- improve match statement
 -- show loading bar and plugin info in vis
--- customize plugins ( set variables etc )
