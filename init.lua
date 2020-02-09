ded565fa44ada4dd43750707ae085ca4ecce479c
Erlend Fagerheim <erlendf80@gmail.com>
2020-02-10 00:25:39 +0100

    update command WIP

--- init.lua
+++ init.lua
@@ -13,22 +13,40 @@ function file_exists(path)
 	end
 end	
 
--- clone + require plugins ( set in visrc.lua )
-if plugins then 
-	for i, url in ipairs(plugins) do
-		local name_ext = url:match('([^/]+)$')
-		local name = name_ext:match('(.+)%..+')
-		local path = plug_path .. name
-		if file_exists(path) then
-			--print('\27[H\27[2J'..'updating plugin ' .. name .. '..')
-			--os.execute('git -C ' .. path .. ' pull --depth=1 --quiet 2> /dev/null')
-		else 
-			print('\27[H\27[2J'..'loading plugin ' .. name .. '..')
-			os.execute('git -C ' .. plug_path .. ' clone --depth=1 ' .. url .. ' --quiet 2> /dev/null')
+function iterate_plugins(operation)
+	if plugins then 
+		for i, url in ipairs(plugins) do
+			local name_ext = url:match('([^/]+)$')
+			local name = name_ext:match('(.+)%..+')
+			local path = plug_path .. name
+			operation(url, name, path)
 		end
-		require(plug_dir .. name)
 	end
 end
+
+function clone_and_require_plugins(url, name, path)
+	if not file_exists(path) then
+		--print('\27[H\27[2J'..'loading plugin ' .. name .. '..')
+		vis:command(string.format(":!echo -ne '\\033[H\\033[2J%s%s'", 'loading plugin ', name))
+		os.execute('git -C ' .. plug_path .. ' clone --depth=1 ' .. url .. ' --quiet 2> /dev/null')
+	end
+	require(plug_dir .. name)
+end
+
+function update_plugins(url, name, path)
+	vis:info('updating plugin ' .. name)
+	os.execute('git -C ' .. path .. ' pull --depth=1 --quiet 2> /dev/null')
+	--os.execute('git -C ' .. path .. ' pull')
+end
+
+vis:command_register("plug-update", function(argv, force, win, selection, range)
+	-- could spesify which plugin to update later
+	--vis:info("update plugins")
+	iterate_plugins(update_plugins)
+	return true
+end)
+
+iterate_plugins(clone_and_require_plugins)
 
 return M
 
