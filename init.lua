c2b4447b9dedbf7a6f9756f2b72318c9c1830d4e
Erlend Fagerheim <erlendf80@gmail.com>
2020-02-11 15:29:52 +0100

    plug-install command | refactor code

--- init.lua
+++ init.lua
@@ -1,57 +1,66 @@
 local M = {}
 
--- plugins directory
-local plug_dir = 'plugins/'
+local plugins_dir = 'plugins/'
+local plugins_path = os.getenv('HOME') .. '/.config/vis/plugins/'
 
--- plugins path
-local plug_path = os.getenv('HOME') .. '/.config/vis/plugins/'
-
-function file_exists(path)
+function exists(path)
 	local f = io.open(path)
 	if f == nil then return false
 	else f:close() return true 
 	end
 end	
 
-function iterate_plugins(operation)
+function iterate(operation)
 	if plugins then 
 		for i, url in ipairs(plugins) do
 			local name_ext = url:match('([^/]+)$')
 			local name = name_ext:match('(.+)%..+')
-			local path = plug_path .. name
+			local path = plugins_path .. name
 			operation(url, name, path)
 		end
 	end
 end
 
-function clone_and_require_plugins(url, name, path)
-	if not file_exists(path) then
-		--print('\27[H\27[2J'..'loading plugin ' .. name .. '..')
-		vis:command(string.format(":!echo -ne '\\033[H\\033[2J%s%s'", 'loading plugin ', name))
-		os.execute('git -C ' .. plug_path .. ' clone --depth=1 ' .. url .. ' --quiet 2> /dev/null')
+function plugins_install(url, name, path)
+	if not exists(path) then
+		vis:info('plug -> install ' .. name)
+		os.execute('git -C ' .. plugins_path .. ' clone ' .. url .. ' --quiet 2> /dev/null')
 	end
-	require(plug_dir .. name)
 end
 
-function update_plugins(url, name, path)
-	vis:info('updating plugin ' .. name)
-	os.execute('git -C ' .. path .. ' pull --depth=1 --quiet 2> /dev/null')
-	--os.execute('git -C ' .. path .. ' pull')
+function plugins_update(url, name, path)
+	if exists(path) then
+		vis:info('plug -> updating ' .. name)
+		os.execute('git -C ' .. path .. ' pull --depth=1 --quiet 2> /dev/null')
+	end
 end
 
-vis:command_register("plug-update", function(argv, force, win, selection, range)
-	-- could spesify which plugin to update later
-	--vis:info("update plugins")
-	iterate_plugins(update_plugins)
+function plugins_require(url, name, path)
+	if exists(path) then
+		require(plugins_dir .. name)
+	end
+end
+
+vis:command_register("plug-install", function(argv, force, win, selection, range)
+	iterate(plugins_install)
+	vis:info('plugins installed')
 	return true
 end)
 
-iterate_plugins(clone_and_require_plugins)
+vis:command_register("plug-update", function(argv, force, win, selection, range)
+	iterate(plugins_update)
+	vis:info('plugins updated')
+	return true
+end)
+
+iterate(plugins_require)
 
 return M
 
 -- TODO's
+-- fix vis:info not showing up before after command
 -- configure plugin folder
 -- commands to fetch and update plugins
 -- improve match statement
 -- show loading bar and plugin info in vis
+-- spesify which plugin to update?
