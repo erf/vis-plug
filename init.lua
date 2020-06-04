2568e2e4d0fbab74becb97807943d16fcdd1760a
Erlend Fagerheim <erlendf80@gmail.com>
2020-06-04 11:40:58 +0200

    write message after command | improve messages | use single quotes

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-local visrc, err = package.searchpath("visrc", package.path)
+local visrc, err = package.searchpath('visrc', package.path)
 assert(not err)
-local visrc_path = visrc:match("(.*/)") 
+local visrc_path = visrc:match('(.*/)') 
 assert(visrc_path)
 
 local plug_path = visrc_path ..'plugins'
@@ -18,7 +18,7 @@ end
 function iterate(op, args)
 	if not plugins then return end
 	for url, file in pairs(plugins) do
-		local name = url:match(".*%/(.*)%..*")
+		local name = url:match('.*%/(.*)%..*')
 		local path = plug_path .. '/' .. name
 		op(url, file, name, path, args)
 	end
@@ -26,21 +26,21 @@ end
 
 function plug_install(url, file, name, path, args)
 	if exists(path) then
-		vis:message(name .. " is already installed")
+		vis:message(name .. ' is already installed')
 	else 
-		vis:message(name)
-		vis:redraw()
 		os.execute('git -C ' .. plug_path .. ' clone ' .. url .. ' --quiet 2> /dev/null')
+		vis:message(name .. ' installed')
+		vis:redraw()
 	end
 end
 
 function plug_update(url, file, name, path, args)
 	if exists(path) then
-		vis:message(name)
-		vis:redraw()
 		os.execute('git -C ' .. path .. ' pull --quiet 2> /dev/null')
+		vis:message(name .. ' updated')
+		vis:redraw()
 	else
-		vis:message(name .. " does not exists ( call plug-install )")
+		vis:message(name .. ' is not installed (call plug-install)')
 	end 
 end
 
@@ -50,7 +50,7 @@ function plug_require(url, file, name, path, args)
 end
 
 function plug_name(url, file, name, path, list)
-	vis:message(name .. ' ( ' .. url .. ' ) ')
+	vis:message(name .. ' (' .. url .. ')')
 end
 
 function plug_count()
@@ -60,7 +60,7 @@ function plug_count()
 	return count
 end
 
-vis:command_register("plug-install", function(argv, force, win, selection, range)
+vis:command_register('plug-install', function(argv, force, win, selection, range)
 	if not exists(plug_path) then os.execute('mkdir -p ' .. plug_path) end
 	vis:message('plug install (' .. plug_count() .. ')')
 	iterate(plug_install, nil)
@@ -68,14 +68,14 @@ vis:command_register("plug-install", function(argv, force, win, selection, range
 	return true
 end)
 
-vis:command_register("plug-update", function(argv, force, win, selection, range)
+vis:command_register('plug-update', function(argv, force, win, selection, range)
 	vis:message('plug update (' .. plug_count() .. ')')
 	iterate(plug_update, nil)
 	vis:message('done')
 	return true
 end)
 
-vis:command_register("plug-list", function(argv, force, win, selection, range)
+vis:command_register('plug-list', function(argv, force, win, selection, range)
 	vis:message('plug list (' .. plug_count() .. ')')
 	iterate(plug_name, list)
 	vis:message('done')
